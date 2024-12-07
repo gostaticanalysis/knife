e677d710f54429bd0fe0c7dfecbf64d9f2f2e25c
Takuya Ueda <uedatakuya@gmail.com>
2024-12-07 16:30:22 +0000

    Fix a build error

--- knife.go
+++ knife.go
@@ -64,7 +64,7 @@ func (k *Knife) Execute(w io.Writer, pkg *packages.Package, tmpl interface{}, op
 	case []byte:
 		tmplStr = string(tmpl)
 	case io.Reader:
-		b, err := os.ReadAll(tmpl)
+		b, err := io.ReadAll(tmpl)
 		if err != nil {
 			return fmt.Errorf("cannnot read template: %w", err)
 		}
